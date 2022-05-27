5d64a502732abd5ccae140646df3fd2fd70332f1
Omarrhassan1 <73880319+Omarrhassan1@users.noreply.github.com>
2022-05-28 01:43:29 +0200

    Create solver.py

---
+++ solver.py
@@ -0,0 +1,33 @@
+def count(seq):
+    return sum(bool(x) for x in seq)
+
+def first(iterable, default=None):
+    
+    try:
+        return iterable[0]
+    except IndexError:
+        return default
+    except TypeError:
+        return next(iterable, default)
+
+def AC3(csp, queue=None, removals=None): #Arc consistency
+    if queue is None:
+        queue = [(Xi, Xk) for Xi in csp.variables for Xk in csp.neighbors[Xi]]
+    csp.support_pruning()
+    while queue:
+        (Xi, Xj) = queue.pop()
+        if revise(csp, Xi, Xj, removals):
+            if not csp.curr_domains[Xi]:
+                return False
+            for Xk in csp.neighbors[Xi]:
+                if Xk != Xj:
+                    queue.append((Xk, Xi))
+    return True
+
+def revise(csp, Xi, Xj, removals):
+    revised = False
+    for x in csp.curr_domains[Xi][:]:
+        if all(not csp.constraints(Xi, x, Xj, y) for y in csp.curr_domains[Xj]):
+            csp.prune(Xi, x, removals)
+            revised = True
+    return revised
