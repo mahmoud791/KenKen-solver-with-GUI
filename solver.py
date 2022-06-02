1370ef68950cc6efc95f7a368870807de37b77bf
Omarrhassan1 <73880319+Omarrhassan1@users.noreply.github.com>
2022-06-02 17:02:07 +0200

    Update solver.py

--- solver.py
+++ solver.py
@@ -31,3 +31,56 @@ def revise(csp, Xi, Xj, removals):
             csp.prune(Xi, x, removals)
             revised = True
     return revised
+    
+def first_unassigned_variable(assignment, csp):
+    return first([var for var in csp.variables if var not in assignment])
+
+
+def unordered_domain_values(var, assignment, csp):
+    return csp.choices(var)
+
+
+def no_inference(csp, var, value, assignment, removals):
+    return True
+
+def forward_checking(csp, var, value, assignment, removals):
+    csp.support_pruning()
+    for B in csp.neighbors[var]:
+        if B not in assignment:
+            for b in csp.curr_domains[B][:]:
+                if not csp.constraints(var, value, B, b):
+                    csp.prune(B, b, removals)
+            if not csp.curr_domains[B]:
+                return False
+    return True
+
+
+def mac(csp, var, value, assignment, removals):
+    #mantain arc consistency
+    return AC3(csp, [(X, var) for X in csp.neighbors[var]], removals)
+
+def backtracking_search(csp,
+                        select_unassigned_variable=first_unassigned_variable,
+                        order_domain_values=unordered_domain_values,
+                        inference=no_inference):
+    
+
+    def backtrack(assignment):
+        if len(assignment) == len(csp.variables):
+            return assignment
+        var = select_unassigned_variable(assignment, csp)
+        for value in order_domain_values(var, assignment, csp):
+            if 0 == csp.nconflicts(var, value, assignment):
+                csp.assign(var, value, assignment)
+                removals = csp.suppose(var, value)
+                if inference(csp, var, value, assignment, removals):
+                    result = backtrack(assignment)
+                    if result is not None:
+                        return result
+                csp.restore(removals)
+        csp.unassign(var, assignment)
+        return None
+
+    result = backtrack({})
+    assert result is None or csp.goal_test(result)
+    return result
